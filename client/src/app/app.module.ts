e9758e51cee005dbd3847b76231c2eb82179fd0e
Janne Strand <janne.strand@tieto.com>
2020-04-10 21:10:52 +0300

    angular forms started on cards component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CardsComponent } from './cards/cards.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
     CardsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/cards/cards.component.ts
+++ client/src/app/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'app-cards',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
-  constructor() { }
+  saveForm;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.saveForm = formBuilder.group({question: '', answer: ''});
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(cardData) {
+    console.log(cardData);
   }
 
 }
